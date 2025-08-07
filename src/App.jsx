3b34e18371d1c78a7229800fbcdd0539981ccfa2
sameer4712 <sameerptmc@gmail.com>
2025-08-07 12:58:20 +0530

    7-7-2025 SlideBar

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Header from './components/Header'
 import Slider from './components/Slider'
 
+
 function App() {
 
   return (
--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,65 +1,93 @@
-import React from 'react'
+import Avenger from './../assets/Images/avenger.jpg'
+import LionKing from './../assets/Images/the lion king.jpg'
+import Frozen2 from './../assets/Images/frozen2.webp'
+import blackpanther from './../assets/Images/blankpanther.webp'
+import avatar from './../assets/Images/avatar.webp'
+import incredible from './../assets/Images/incredible.webp'
+import guardian from './../assets/Images/guardian of the galaxy.webp'
+import toystory from './../assets/Images/toy story.webp'
+import doctorstrange from './../assets/Images/doctorstrange.webp'
+import moana from './../assets/Images/moana.webp'
+import seven from './../assets/Images/seven.webp'
+import fightclub from './../assets/Images/fight club.webp'
+import Titanic from './../assets/Images/titanic.webp'
+import './../index.css'
 
 function Slider() {
     const TitleMovie = [
         {
             title: "Avengers: Endgame",
-            image: "https://image.tmdb.org/t/p/w500/q6725aR8Zs4IwGMXzZT8aC8lh41.jpg",
+            image: Avenger,
             genre: "Action"
         },
         {
             title: "The Lion King",
-            image: "https://image.tmdb.org/t/p/w500/2bXbqYdUdNVa8VIWXVfclP2ICtT.jpg",
+            image: LionKing,
             genre: "Animation"
         },
         {
             title: "Frozen II",
-            image: "https://image.tmdb.org/t/p/w500/pjeMs3yqRmFL3giJy4PMXWZTTPa.jpg",
+            image: Frozen2,
             genre: "Fantasy"
         },
         {
             title: "Black Panther",
-            image: "https://image.tmdb.org/t/p/w500/uxzzxijgPIY7slzFvMotPv8wjKA.jpg",
+            image: blackpanther,
             genre: "Superhero"
         },
         {
             title: "Avatar: The Way of Water",
-            image: "https://image.tmdb.org/t/p/w500/t6HIqrRAclMCA60NsSmeqe9RmNV.jpg",
+            image: avatar,
             genre: "Sci-Fi"
         },
         {
             title: "Incredibles 2",
-            image: "https://image.tmdb.org/t/p/w500/9lFKBtaVIhP7E2Pk0IY1CwTKTMZ.jpg",
+            image: incredible,
             genre: "Animation"
         },
         {
             title: "Guardians of the Galaxy",
-            image: "https://image.tmdb.org/t/p/w500/r7vmZjiyZw9rpJMQJdXpjgiCOk9.jpg",
+            image: guardian,
             genre: "Action"
         },
         {
             title: "Toy Story 4",
-            image: "https://image.tmdb.org/t/p/w500/w9kR8qbmQ01HwnvK4alvnQ2ca0L.jpg",
+            image: toystory,
             genre: "Animation"
         },
         {
             title: "Doctor Strange",
-            image: "https://image.tmdb.org/t/p/w500/uGy4DCmM33I7l86W7iCskNkvmLD.jpg",
+            image: doctorstrange,
             genre: "Superhero"
         },
         {
             title: "Moana",
-            image: "https://image.tmdb.org/t/p/w500/2klgJ8lR7D5EAL8PqUEbaGg0bxj.jpg",
+            image: moana,
+            genre: "Adventure"
+        },
+        {
+            title: "Seven",
+            image: seven,
+            genre: "Adventure"
+        },
+        {
+            title: "Fight Club",
+            image: fightclub,
+            genre: "Adventure"
+        },
+        {
+            title: "Titanic",
+            image: Titanic,
             genre: "Adventure"
         }
     ];
 
     return (
-        <div>
+        <div className='flex  w-full px-6 scrollbar-hide snap-x snap-mandatory scroll-smooth'>
             {TitleMovie.map((movie, index) => (
-                <div key={index} >
-                    <div className='gap-8'>
-                        <img src="movie.image" />
+                <div key={index} className='min-w-[800px] flex-shrink-0 ' >
+                    <div className='mr-6'>
+                        <img src={movie.image} className='w-full h-[400px] object-cover mr-5'/>
                         <h3>{movie.title}</h3>
                         <p>{movie.genre}</p>
                     </div>
