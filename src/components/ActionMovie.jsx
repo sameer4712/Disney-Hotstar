10ca7d1fa8713c9252142a3c10003b0f749a592f
sameer4712 <sameerptmc@gmail.com>
2025-08-08 12:03:07 +0530

    movie series finished

--- src/components/ActionMovie.jsx
+++ src/components/ActionMovie.jsx
@@ -5,19 +5,19 @@ import johnwick4 from './../assets/Images/johnWick4.jpg'
 function ActionMovie() {
     const TitleMovie = [
         {
-            title: "John Wick: Chapter:1",
+            title: "John Wick: Chapter 1",
             image: johnwick1,
         },
         {
-            title: "John Wick: Chapter:2",
+            title: "John Wick: Chapter 2",
             image: johnwick2,
         },
         {
-            title: "John Wick: Chapter:3",
+            title: "John Wick: Chapter 3",
             image: johnwick3,
         },
         {
-            title: "John Wick: Chapter:4",
+            title: "John Wick: Chapter 4",
             image: johnwick4,
         },
     ]
@@ -27,8 +27,9 @@ function ActionMovie() {
             <div className='w-full flex justify-around px-14 shadow-2xl shadow-gray-600'>
 
                 {TitleMovie.map((item) => (
-                    <div>
-                        <img src={item.image} className='object-cover h-[500px] rounded-2xl' />
+                    <div className='hover:scale-90 hover:text-red-600 transition-all duration-150 ease-in-out'>
+                        <img src={item.image} className='object-cover h-[500px] rounded-2xl border-2 border-gray-500' />
+                        <h2 className='text-center mt-4 text-3xl font-bold'>{item.title}</h2>
                     </div>
                 ))}
             </div>
--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -89,9 +89,9 @@ function Slider() {
 
             <div id='slider' className='flex  w-full overflow-x-scroll px-11 scrollbar-hide snap-x snap-mandatory scroll-smooth' >
                 {TitleMovie.map((movie, index) => (
-                    <div key={index} className='min-w-[1400px] flex-shrink-0 py-4' >
-                        <div className='mr-6 mb-4' >
-                            <img src={movie.image} className='w-full h-[400px] object-left-top mr-5 rounded-3xl hover:border-[3px] border-gray-400 transition-all duration-100 ease-in-out' />
+                    <div key={index} className='flex-shrink-0 py-4' >
+                        <div className='mr-6 mb-10' >
+                            <img src={movie.image} className='w-full h-[400px] object-left-top mr-5 rounded-3xl hover:border-[3px] border-gray-400 transition-all duration-150 ease-in-out' />
                             <h3 className='text-center text-teal-200 text-4xl mb-2 py-7 font-bold'>{movie.title}</h3>
                         </div>
 
